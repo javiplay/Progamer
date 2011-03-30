27b861d832e4b43311a07b4474d8b1fa320b8147
J.JavierAsensio <J.JavierAsensio@fcd94a1d-f216-129b-b1d1-a70178862966>
2011-03-30 16:20:01 +0000

    probando listeners para el cursor y visitors para el AST... esto esta a punto de comenzarrrr!!!!

--- SuperMarioCode/src/supermariocode/views/MarioCodeView.java
+++ SuperMarioCode/src/supermariocode/views/MarioCodeView.java
@@ -1,6 +1,17 @@
 package supermariocode.views;
 
 
+import java.util.ArrayList;
+
+
+import org.eclipse.jdt.core.ICompilationUnit;
+import org.eclipse.jdt.core.IPackageFragment;
+import org.eclipse.jdt.core.IPackageFragmentRoot;
+import org.eclipse.jdt.core.JavaCore;
+import org.eclipse.jdt.core.dom.AST;
+import org.eclipse.jdt.core.dom.ASTParser;
+import org.eclipse.jdt.core.dom.CompilationUnit;
+import org.eclipse.jdt.core.dom.MethodDeclaration;
 import org.eclipse.jface.action.Action;
 import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.action.IToolBarManager;
@@ -9,12 +20,16 @@ import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IDoubleClickListener;
 import org.eclipse.jface.viewers.ISelection;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.custom.CaretEvent;
+import org.eclipse.swt.custom.CaretListener;
+import org.eclipse.swt.custom.StyledText;
 import org.eclipse.swt.events.MouseEvent;
 import org.eclipse.swt.events.MouseListener;
 import org.eclipse.swt.graphics.GC;
 import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.widgets.Canvas;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Control;
 import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IActionBars;
@@ -26,11 +41,16 @@ import org.eclipse.ui.IWorkbenchPart;
 import org.eclipse.ui.PlatformUI;
 import org.eclipse.ui.part.ViewPart;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.resources.IProject;
+import org.eclipse.core.resources.IResource;
 import org.eclipse.core.resources.IResourceChangeEvent;
 import org.eclipse.core.resources.IResourceChangeListener;
 import org.eclipse.core.resources.IResourceDelta;
+import org.eclipse.core.resources.IResourceDeltaVisitor;
 import org.eclipse.core.resources.IWorkspace;
+import org.eclipse.core.resources.IWorkspaceRoot;
 import org.eclipse.core.resources.ResourcesPlugin;
+import org.eclipse.core.runtime.CoreException;
 import org.eclipse.jface.text.Document;
 import org.eclipse.jface.text.DocumentEvent;
 import org.eclipse.jface.text.IDocument;
@@ -155,14 +175,56 @@ public class MarioCodeView extends ViewPart implements ISelectionListener {
 	{
 		@Override
 		public void resourceChanged(IResourceChangeEvent event) {
-				// TODO Auto-generated method stub					
-			if (event.getType() ==  IResourceChangeEvent.POST_CHANGE) {				
+				// TODO Auto-generated method stub
+			
+			if (event.getType() ==  IResourceChangeEvent.POST_CHANGE) {
+				
+				final ArrayList<IResource> changed = new ArrayList<IResource>();
+				IResourceDelta rootDelta = event.getDelta();
+
+				//definimos el visitante
+				IResourceDeltaVisitor visitor = new IResourceDeltaVisitor() {
+		            public boolean visit(IResourceDelta delta) {
+		               //only interested in changed resources (not added or removed)
+		               if (delta.getKind() != IResourceDelta.CHANGED)
+		                  return true;
+		               //only interested in content changes
+		               if ((delta.getFlags() & IResourceDelta.CONTENT) == 0)
+		                  return true;
+		               IResource resource = delta.getResource();
+		               //only interested in files with the "java" extension
+		               if (resource.getType() == IResource.FILE && 
+						"java".equalsIgnoreCase(resource.getFileExtension())) {
+		                  changed.add(resource);
+		               }
+		               return true;
+		            }
+		         };
+		         try {
+		             rootDelta.accept(visitor);
+		          } catch (CoreException e) {
+		             //open error dialog with syncExec or print to plugin log file
+		          }
+
+				
+
+		        if (changed.size()>0){
+					System.out.println("Evento resourceChanged (tipo POST_CHANGE)");
+					for (IResource res: changed ) {
+						System.out.println("Resource: "+ res.getName());				
+					}
+					System.out.println("-----------------------------------------");
+		        }
+				
+				
+				// actualización de la interfaz
 				Display disp = control.getDisplay();
 				disp.asyncExec(new Runnable() {					
 					@Override
 					public void run() {
 						label.setText(label.getText()+"\nEvent type: CHANGED");
-						//IResourceDelta rootDelta = event.getDelta();						
+
+										
 					}
 				});					
 			}
@@ -187,13 +249,66 @@ public class MarioCodeView extends ViewPart implements ISelectionListener {
 		manager.add(action2);
 	}
 
+	/**
+	 * Reads a ICompilationUnit and creates the AST DOM for manipulating the
+	 * Java source file
+	 * 
+	 * @param unit
+	 * @return
+	 */
+
+	private static CompilationUnit parse(ICompilationUnit unit) {
+		ASTParser parser = ASTParser.newParser(AST.JLS3);
+		parser.setKind(ASTParser.K_COMPILATION_UNIT);
+		parser.setSource(unit);
+		parser.setResolveBindings(true);
+		return (CompilationUnit) parser.createAST(null); // parse
+	}
 	private void makeActions() {
 		
 		/* Action 1 */
 		action1 = new Action() {
 			public void run() {
 				showMessage("Action 1 executed");
-				myCanvas.setBackgroundImage(image1);
+				myCanvas.setBackgroundImage(image1);	
+				
+				IWorkspace workspace = ResourcesPlugin.getWorkspace();
+				IWorkspaceRoot root = workspace.getRoot();
+				// Get all projects in the workspace
+				IProject[] projects = root.getProjects();
+				// Loop over all projects
+				for (IProject project : projects) {
+					try {
+						if (project.isNatureEnabled("org.eclipse.jdt.core.javanature")) {
+
+							IPackageFragment[] packages = JavaCore.create(project)
+									.getPackageFragments();
+							// parse(JavaCore.create(project));
+							for (IPackageFragment mypackage : packages) {
+								if (mypackage.getKind() == IPackageFragmentRoot.K_SOURCE) {
+									for (ICompilationUnit unit : mypackage
+											.getCompilationUnits()) {
+										// Now create the AST for the ICompilationUnits
+										CompilationUnit parse = parse(unit);
+										MethodVisitor visitor = new MethodVisitor();
+										parse.accept(visitor);
+
+										for (MethodDeclaration method : visitor.getMethods()) {
+											System.out.print("Method name: "
+													+ method.getName()
+													+ " Return type: "
+													+ method.getReturnType2());
+										}
+
+									}
+								}
+
+							}
+						}
+					} catch (CoreException e) {
+						e.printStackTrace();
+					}
+				}
 			}
 		};
 		action1.setText("Action 1");
@@ -238,48 +353,33 @@ public class MarioCodeView extends ViewPart implements ISelectionListener {
 			Class c = part.getClass();			
 			if (c.getName().equals("org.eclipse.jdt.internal.ui.javaeditor.CompilationUnitEditor")) {
 				IEditorPart editor = (IEditorPart) part;
-				IEditorInput input = editor.getEditorInput();
 				
+				StyledText st = (StyledText) editor.getAdapter(Control.class);
 				
-				
-				String text = input.getName() + " should be painted...?";				
-				text += "\n getting its IDocument object...does not work.";				
-				IFile file = (IFile) input.getAdapter(IFile.class);				
-				IDocument doc = (IDocument) file.getAdapter(Document.class);
-				if (doc==null) text+="Document is NULL!!";
-				
-				
-				label.setText(text);
-				
-				/*doc.addDocumentListener(new IDocumentListener() {
-
+				try {
+				st.addCaretListener(new CaretListener() {
+					
 					@Override
-					public void documentAboutToBeChanged(DocumentEvent arg0) {
+					public void caretMoved(CaretEvent event) {
 						// TODO Auto-generated method stub
+						System.out.println(event.caretOffset);
 						
 					}
-
-					@Override
-					public void documentChanged(DocumentEvent arg0) {
-						label.setText(label.getText()+"\nDocument Changed occurred");						
-					}
-					
-											
-					});
-				*/
+				});
+				}
+				catch (NullPointerException e) {
+					System.out.println("nooooo");
 				
-				//file.getContents();
-				input.getPersistable();
+				}
 				
-				
-				
+				IEditorInput input = editor.getEditorInput();
+				System.out.println("Evento de selección sobre un editor java");
+				System.out.println("IWortkbenchPart = "+c.getName());				
+				System.out.println("IEditorInput = "+input.getName());
+				System.out.println("----------------------------------------");
 				
 			}
-			else {
-				label.setText("Hello World! Open or select a valid java file");
-			}							
 		}		
-		label.setText(label.getText()+"\n-----\n"+getSite().getPage().getEditorReferences().length+" opened editors.");
-		
+		System.out.println(getSite().getPage().getEditorReferences().length+" editores abiertos.");
 	}
 }
